fea1140af0878861c83b33e0f29afd8dc30e6394
KaeHyun <sally980330@naver.com>
2023-05-18 01:56:59 +0900

    Initial commit

---
+++ app/src/main/java/edu/skku/cs/semester/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.skku.cs.semester
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
