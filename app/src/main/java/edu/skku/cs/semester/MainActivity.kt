0f7fd2ecc2d4d55b65b2133bc35d0648c240e06a
KaeHyun <sally980330@naver.com>
2023-05-31 02:07:42 +0900

    [FEAT] Nav Buttons

--- app/src/main/java/edu/skku/cs/semester/MainActivity.kt
+++ app/src/main/java/edu/skku/cs/semester/MainActivity.kt
@@ -2,12 +2,24 @@ package edu.skku.cs.semester
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 //        https://mimisongsong.tistory.com/33
+        val homeButton = findViewById<ImageButton>(R.id.homeButton)
+        homeButton.setOnClickListener {
+            // 클릭 이벤트에 대한 동작을 여기에 구현합니다.
+            // 예: 버튼이 클릭되었을 때 수행할 작업
+            Toast.makeText(this, "Home button clicked", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
